7a2de4bf8b24c2df54d4c3310e59da23f4a80d6d
RoMan-robots <romkozhan@gmail.com>
2025-03-26 14:18:17 +0200

    rock game scissors done

--- miniGames/rockPaperScissors/script.js
+++ miniGames/rockPaperScissors/script.js
@@ -5,17 +5,23 @@ const computerChoiceEl = document.querySelector(".computer-choice");
 const userPointsEl = document.querySelector(".user-points");
 const computerPointsEl = document.querySelector(".computer-points");
 
+const choicesMap = {
+    rock: "камінь",
+    paper: "папір",
+    scissor: "ножиці"
+};
+
 let userPoints= 0;
 let computerPoints = 0;
 
 imgEls.forEach((img) => {
-  img.addEventListener("click", () => {
-    const computerTurn = computerChoice();
-    const result = gamePlay(img.id, computerTurn);
-    userChoiceEl.textContent = img.id;
-    computerChoiceEl.textContent = computerTurn;
-    resultEl.textContent = result;
-  });
+    img.addEventListener("click", () => {
+        const computerTurn = computerChoice();
+        const result = gamePlay(img.id, computerTurn);
+        userChoiceEl.textContent = choicesMap[img.id];
+        computerChoiceEl.textContent = choicesMap[computerTurn];
+        resultEl.textContent = result;
+    });
 });
 
 // Computer randomly pick one choice from the given choices
@@ -25,21 +31,55 @@ function computerChoice() {
   return choices[randomChoice];
 }
 
+function reward() { 
+    const token = localStorage.getItem('token');
+    if (!token) {
+        console.log('Invalid score')
+        return
+    }
+    let score = 10
+    fetch('/checkAuth', {
+        headers: {
+            'Authorization': `Bearer ${token}`,
+        },
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (!data.isAuthenticated) {
+                return
+            }
+            fetch(`/miniGames/reward/${score}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`,
+                },
+            })
+            .catch(error => {
+                console.error(error)
+            })
+        })
+}
+
 // it return result by checking user and computer selection
 function gamePlay(userSelection, computerSelection) {
-  if (userSelection === computerSelection) {
-    return "It is a tie..!";
-  } else if (
-    (userSelection === "rock" && computerSelection === "scissor") ||
-    (userSelection === "paper" && computerSelection === "rock") ||
-    (userSelection === "scissor" && computerSelection === "paper")
-  ) {
-    userPoints++;
-    userPointsEl.textContent = userPoints;
-    return "Hurrah! You win..! " + userSelection + " beats " + computerSelection;
-  } else {
-    computerPoints++;
-    computerPointsEl.textContent = computerPoints;
-    return "Oops! You lose...! " + computerSelection + " beats " + userSelection;
-  }
+    const userChoiceUkrainian = choicesMap[userSelection];
+    const computerChoiceUkrainian = choicesMap[computerSelection];
+
+    if (userSelection === computerSelection) {
+        return "Нічия...";
+    } else if (
+        (userSelection === "rock" && computerSelection === "scissor") ||
+        (userSelection === "paper" && computerSelection === "rock") ||
+        (userSelection === "scissor" && computerSelection === "paper")
+    ) {
+        userPoints++;
+        userPointsEl.textContent = userPoints;
+        reward();
+        return `Ви виграли! ${userChoiceUkrainian} перемагає ${computerChoiceUkrainian}`;
+    } else {
+        computerPoints++;
+        computerPointsEl.textContent = computerPoints;
+        return `Ви програли! ${computerChoiceUkrainian} перемагає ${userChoiceUkrainian}`;
+    }
 }
