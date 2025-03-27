c85bf0913835756113e7373c0cc7acfbfeaf1591
RoMan-robots <romkozhan@gmail.com>
2025-03-27 18:56:06 +0200

    db migration

---
+++ migrations/20250327164347-db.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db) {
+  return Promise.all([
+    db.dropTable('purchases', { ifExists: true }),
+    db.dropTable('store_items', { ifExists: true }),
+    db.dropTable('users', { ifExists: true }),
+    db.createTable('users', {
+      id: { type: 'serial', primaryKey: true },
+      login: { type: 'varchar', unique: true },
+      email: { type: 'varchar', unique: true },
+      password: { type: 'varchar' },
+      balance: { type: 'float' }
+    }),
+    db.createTable('store_items', {
+      id: { type: 'serial', primaryKey: true },
+      name: { type: 'varchar', unique: true },
+      description: { type: 'varchar' },
+      price: { type: 'float' },
+      image_url: { type: 'varchar' },
+      category: { type: 'varchar' },
+      rarity: { type: 'varchar' },
+      is_golden: { type: 'boolean', defaultValue: false }
+    }),
+    db.createTable('purchases', {
+      id: { type: 'serial', primaryKey: true },
+      customer: { type: 'int', notNull: true, foreignKey: { name: 'fk_customer', table: 'users', mapping: 'id' } },
+      bought_item: { type: 'int', notNull: true, foreignKey: { name: 'fk_bought_item', table: 'store_items', mapping: 'id' } },
+      date: { type: 'date' }
+    })
+  ]);
+};
+
+exports.down = function(db) {
+  return Promise.all([
+    db.dropTable('purchases', { ifExists: true }),
+    db.dropTable('store_items', { ifExists: true }),
+    db.dropTable('users', { ifExists: true })
+  ]);
+};
+
+exports._meta = {
+  "version": 1
+};
--- model/db.js
+++ model/db.js
@@ -1,13 +1,20 @@
 const { Pool } = require('pg');
 require('dotenv').config('../.env');
+const fs = require('fs');
 
-const pool = new Pool({
-    user: process.env.DB_USER,
+const rawConfig = fs.readFileSync('database.json');
+const config = JSON.parse(rawConfig);
+
+const dbConfig = {
+    ...config.dev,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
     max: 10,
-});
+};
 
-module.exports = {pool};
+const pool = new Pool(dbConfig);
+
+module.exports = { pool };
